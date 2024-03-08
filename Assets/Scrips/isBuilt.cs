826e48ed2f0bdfa88e20fae9868f27323aba380d
SCU Imaginarium (GLaDOS) <>
2024-03-08 12:57:37 -0800

    reworked original room, attach points modified
    need to figure out work around for triggers on every block besides red

--- Assets/Scrips/isBuilt.cs
+++ Assets/Scrips/isBuilt.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class isBuilt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] GameObject Floor;
+    [SerializeField] GameObject redSocket;
     void Update()
     {
-        
+        Transform redTransform = null;
+        if (redSocket.GetComponent<XRSocketInteractor>().attachTransform == redTransform)
+        {
+            Floor.GetComponent<Rigidbody>().useGravity = false;
+        }
     }
 }
-
-
-/*
- * pseudocode
- * if red
-*/
