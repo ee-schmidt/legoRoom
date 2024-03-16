b96860394fbed2e18c8c6e6e8d725d62bc722d3d
SCU Imaginarium (Zagreus) <125588221+ee-schmidt@users.noreply.github.com>
2024-03-15 17:11:35 -0700

    added sound and ui

--- Assets/Scrips/isBuilt.cs
+++ Assets/Scrips/isBuilt.cs
@@ -1,12 +1,16 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+
 
 public class isBuilt : MonoBehaviour
 {
     [SerializeField] GameObject Floor;
     [SerializeField] GameObject redSocket;
+    [SerializeField] int nextScene;
+
     void Update()
     {
         Transform redTransform = null;
@@ -15,4 +19,14 @@ public class isBuilt : MonoBehaviour
             Floor.GetComponent<Rigidbody>().useGravity = false;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == goFreeBuild)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
 }
+
+*/
