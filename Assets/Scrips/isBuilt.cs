36bd1c65b10fc0b70e7fdcf4cc8060dffe3fd51f
SCU Imaginarium (GLaDOS) <>
2024-03-16 13:58:59 -0700

    added timer

--- Assets/Scrips/isBuilt.cs
+++ Assets/Scrips/isBuilt.cs
@@ -1,32 +1,42 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Threading;
 
 
 public class isBuilt : MonoBehaviour
 {
+    //deleting the floor variable declaration
     [SerializeField] GameObject Floor;
-    [SerializeField] GameObject redSocket;
-    [SerializeField] int nextScene;
+  
+
+
+    //timer variable declaration
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    
 
     void Update()
     {
-        Transform redTransform = null;
-        if (redSocket.GetComponent<XRSocketInteractor>().attachTransform == redTransform)
+        if (remainingTime > 0)
         {
-            Floor.GetComponent<Rigidbody>().useGravity = false;
+            remainingTime -= Time.deltaTime;
         }
+        else if(remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerText.color = Color.red;
+            SceneManager.LoadScene(0);
+        }
+        
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == goFreeBuild)
-        {
-            SceneManager.LoadScene(nextScene);
-        }
-    }
+    
 }
-
-*/
