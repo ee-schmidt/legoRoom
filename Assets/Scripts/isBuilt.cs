e7af132887b01cf3f0d385f83be0599692161667
SCU Imaginarium (GLaDOS) <125588221+ee-schmidt@users.noreply.github.com>
2024-03-17 01:03:44 -0700

    tp & ceiling

---
+++ Assets/Scripts/isBuilt.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Threading;
+using System;
+
+
+public class isBuilt : MonoBehaviour
+{
+    //timer variable declaration
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    //teleport to winning screen variable declaration
+    private bool teleportOccurred = false;
+
+    void Update()
+    {
+        if (!teleportOccurred)
+        {
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            else if (remainingTime == 0 || remainingTime < 0)
+            {
+                remainingTime = 0;
+                timerText.color = Color.red;
+                int newScene = UnityEngine.Random.Range(0, 4);
+                SceneManager.LoadScene(newScene);
+            }
+
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+    
+    public void OnTeleportation()
+    {
+        teleportOccurred = true;
+        SceneManager.LoadScene(4);
+    }
+}
